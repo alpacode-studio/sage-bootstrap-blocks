be666446df05f0bce3c45fdb6770070dd7f0439d
FrancescoCorbosiero <francesco.corbosiero00@gmail.com>
2025-07-10 03:30:30 +0200

    First stable

--- wp-content/cache/acorn/framework/views/340ed6a0796566441d5447c701b3204f.php
+++ wp-content/cache/acorn/framework/views/340ed6a0796566441d5447c701b3204f.php
@@ -3,9 +3,31 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    
+    <!-- Vendor CSS - Simple approach -->
+    <link href="<?php echo e(get_template_directory_uri()); ?>/vendors/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo e(get_template_directory_uri()); ?>/vendors/bootstrap-icons/font/bootstrap-icons.min.css" rel="stylesheet">
+    <link href="<?php echo e(get_template_directory_uri()); ?>/vendors/glightbox/glightbox.min.css" rel="stylesheet">
+    <link href="<?php echo e(get_template_directory_uri()); ?>/vendors/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="<?php echo e(get_template_directory_uri()); ?>/vendors/aos/aos.css" rel="stylesheet">
+    
+    <!-- Theme switcher -->
+    <script>
+      (function() {
+        const storedTheme = localStorage.getItem('theme') || 'light';
+        document.documentElement.setAttribute('data-bs-theme', storedTheme);
+      })();
+    </script>
+
     <?php (do_action('get_header')); ?>
     <?php (wp_head()); ?>
 
+    
     <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
   </head>
 
@@ -33,8 +55,18 @@
       <?php echo $__env->make('sections.footer', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
     </div>
 
+    <!-- Vendors JS -->
+    <script src="<?php echo e(get_template_directory_uri()); ?>/vendors/gsap/gsap.min.js"></script>
+    <script src="<?php echo e(get_template_directory_uri()); ?>/vendors/purecounter/purecounter.js"></script>
+    <script src="<?php echo e(get_template_directory_uri()); ?>/vendors/imagesloaded/imagesloaded.pkgd.min.js"></script>
+    <script src="<?php echo e(get_template_directory_uri()); ?>/vendors/isotope/isotope.pkgd.min.js"></script>
+
+    <script src="<?php echo e(get_template_directory_uri()); ?>/vendors/bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo e(get_template_directory_uri()); ?>/vendors/glightbox/glightbox.min.js"></script>
+    <script src="<?php echo e(get_template_directory_uri()); ?>/vendors/swiper/swiper-bundle.min.js"></script>
+    <script src="<?php echo e(get_template_directory_uri()); ?>/vendors/aos/aos.js"></script>
+
     <?php (do_action('get_footer')); ?>
     <?php (wp_footer()); ?>
   </body>
-</html>
-<?php /**PATH C:\xampp\htdocs\alpacode-bootstrap\wp-content\themes\alpacode-bootstrap-theme\resources\views/layouts/app.blade.php ENDPATH**/ ?>
+</html><?php /**PATH C:\xampp\htdocs\alpacode-bootstrap\wp-content\themes\alpacode-bootstrap-theme\resources\views/layouts/app.blade.php ENDPATH**/ ?>
